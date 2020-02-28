b80653365dfd0ea9c79c177409447cab04818baa
hamid bh <hamidbahram2@gmail.com>
2020-02-28 17:37:33 +0330

    create status in models

--- todo/todoapp/admin.py
+++ todo/todoapp/admin.py
@@ -1,15 +1,45 @@
 from django.contrib import admin
 from . import models
-# Register your models here.
+
+
+class CategoryTabolarInline(admin.TabularInline):
+    model = models.Status
+
+
+@admin.register(models.Status)
+class AdminStatus(admin.ModelAdmin):
+    pass
+
 
 @admin.register(models.Category)
 class AdminCategory(admin.ModelAdmin):
     pass
 
-@admin.register(models.Task)
-class AdminCategory(admin.ModelAdmin):
-    pass
 
 @admin.register(models.File)
-class AdminCategory(admin.ModelAdmin):
+class AdminFile(admin.ModelAdmin):
     pass
+
+
+@admin.register(models.Task)
+class AdminTask(admin.ModelAdmin):
+    list_display = ('title', 'create', 'user')
+    list_editable = ('title',)
+    list_display_links = ('create',)
+    list_filter = ('create', 'status')
+    search_fields = ('title', 'content',)
+    # exclude            = ('user',)
+    date_hierarchy = ('create')
+    # actions          = [Status]
+    inlines = [CategoryTabolarInline]
+    fieldsets = (
+        ('information', {
+            "fields": ('title', 'content', 'name_file')
+        }),
+        ('time', {
+            "fields": ('due_date',)
+        }),
+        ('detail information', {
+            "fields": ('category', 'user',)
+        }),
+    )
--- todo/todoapp/models.py
+++ todo/todoapp/models.py
@@ -2,8 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
+
 class Category(models.Model):
-    name = models.CharField(max_length=100) 
+    name = models.CharField(max_length=100)
 
     class Meta:
         verbose_name = ("Category")
@@ -14,28 +15,49 @@ class Category(models.Model):
 
 
 class File(models.Model):
-    name_files   = models.FileField(upload_to='uploads/%Y/%m/%d/')
+    name_files = models.FileField(upload_to='uploads/%Y/%m/%d/')
 
     class Meta:
         verbose_name = ("File")
         verbose_name_plural = ("Files")
 
     def __str__(self):
-        return "{}".format(self.name_files) 
+        return "{}".format(self.name_files)
+
 
 class Task(models.Model):
-    title       = models.CharField(max_length=100)
-    content     = models.TextField()
-    name_file   = models.ForeignKey(File, on_delete=models.CASCADE) 
-    create      = models.DateTimeField(auto_now_add=True, auto_now=False)
-    update      = models.DateTimeField(auto_now_add=False, auto_now=True)
-    due_date    = models.DateField(default=timezone.now().strftime("%Y-%m-%d"))
-    category    = models.ForeignKey(Category, on_delete=models.CASCADE)
-    user        = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    name_file = models.ForeignKey(File, on_delete=models.CASCADE)
+    create = models.DateTimeField(auto_now_add=True, auto_now=False)
+    update = models.DateTimeField(auto_now_add=False, auto_now=True)
+    due_date = models.DateField(default=timezone.now().strftime("%Y-%m-%d"))
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
     class Meta:
-        ordering = ["-create"] 
+        ordering = ["-create"]
 
     def __str__(self):
-        return "{}".format(self.title )
-   
+        return "{}".format(self.title)
+
+
+class Status(models.Model):
+    TODO = 1
+    DOING = 2
+    DONE = 3
+
+    types = (
+        (TODO, 'todo'),
+        (DOING, 'doing'),
+        (DONE, 'done'),
+    )
+    status_type = models.SmallIntegerField(choices=types)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, null=True)
+
+    class Meta:
+        verbose_name = ("Status")
+        verbose_name_plural = ("Status")
+
+    # def __str__(self):
+    #     return "{}".format(self.types)
